db76bd306718882e3fffa63e264f5d8e8553c262
ArdenButterfield <76457489+ArdenButterfield@users.noreply.github.com>
2023-04-26 22:05:19 -0700

    fix error in mac and linux builds

---
+++ Source/BinaryData.h
@@ -0,0 +1,237 @@
+/* =========================================================================================
+
+   This is an auto-generated file: Any edits you make may be overwritten!
+
+*/
+
+#pragma once
+
+namespace BinaryData
+{
+    extern const char*   fishghost0_png;
+    const int            fishghost0_pngSize = 128681;
+
+    extern const char*   fishghost1_png;
+    const int            fishghost1_pngSize = 129334;
+
+    extern const char*   fishghost2_png;
+    const int            fishghost2_pngSize = 131310;
+
+    extern const char*   fishghost3_png;
+    const int            fishghost3_pngSize = 132465;
+
+    extern const char*   fishghost4_png;
+    const int            fishghost4_pngSize = 129682;
+
+    extern const char*   fishghost5_png;
+    const int            fishghost5_pngSize = 122373;
+
+    extern const char*   fishghost6_png;
+    const int            fishghost6_pngSize = 113789;
+
+    extern const char*   fishghost7_png;
+    const int            fishghost7_pngSize = 96856;
+
+    extern const char*   fishghost8_png;
+    const int            fishghost8_pngSize = 85009;
+
+    extern const char*   fishghost9_png;
+    const int            fishghost9_pngSize = 73702;
+
+    extern const char*   fishghost10_png;
+    const int            fishghost10_pngSize = 72145;
+
+    extern const char*   fishghost11_png;
+    const int            fishghost11_pngSize = 79393;
+
+    extern const char*   fishghost12_png;
+    const int            fishghost12_pngSize = 93230;
+
+    extern const char*   fishghost13_png;
+    const int            fishghost13_pngSize = 111984;
+
+    extern const char*   fishghost14_png;
+    const int            fishghost14_pngSize = 128093;
+
+    extern const char*   fishghost15_png;
+    const int            fishghost15_pngSize = 135942;
+
+    extern const char*   fishghost16_png;
+    const int            fishghost16_pngSize = 135640;
+
+    extern const char*   fishghost17_png;
+    const int            fishghost17_pngSize = 134747;
+
+    extern const char*   fishghost18_png;
+    const int            fishghost18_pngSize = 135060;
+
+    extern const char*   fishghost19_png;
+    const int            fishghost19_pngSize = 137320;
+
+    extern const char*   fishghost20_png;
+    const int            fishghost20_pngSize = 139261;
+
+    extern const char*   fishghost21_png;
+    const int            fishghost21_pngSize = 146247;
+
+    extern const char*   fishghost22_png;
+    const int            fishghost22_pngSize = 146666;
+
+    extern const char*   fishghost23_png;
+    const int            fishghost23_pngSize = 142718;
+
+    extern const char*   fishghost24_png;
+    const int            fishghost24_pngSize = 131427;
+
+    extern const char*   fishghost25_png;
+    const int            fishghost25_pngSize = 119393;
+
+    extern const char*   fishghost26_png;
+    const int            fishghost26_pngSize = 101329;
+
+    extern const char*   fishghost27_png;
+    const int            fishghost27_pngSize = 92716;
+
+    extern const char*   fishghost28_png;
+    const int            fishghost28_pngSize = 93079;
+
+    extern const char*   fishghost29_png;
+    const int            fishghost29_pngSize = 103478;
+
+    extern const char*   fishghost30_png;
+    const int            fishghost30_pngSize = 118421;
+
+    extern const char*   fishghost31_png;
+    const int            fishghost31_pngSize = 129411;
+
+    extern const char*   fishghost32_png;
+    const int            fishghost32_pngSize = 133035;
+
+    extern const char*   fishghost33_png;
+    const int            fishghost33_pngSize = 135699;
+
+    extern const char*   fishghost34_png;
+    const int            fishghost34_pngSize = 140097;
+
+    extern const char*   fishunique0_png;
+    const int            fishunique0_pngSize = 17762;
+
+    extern const char*   fishunique1_png;
+    const int            fishunique1_pngSize = 21139;
+
+    extern const char*   fishunique2_png;
+    const int            fishunique2_pngSize = 23309;
+
+    extern const char*   fishunique3_png;
+    const int            fishunique3_pngSize = 24718;
+
+    extern const char*   fishunique4_png;
+    const int            fishunique4_pngSize = 24815;
+
+    extern const char*   fishunique5_png;
+    const int            fishunique5_pngSize = 23938;
+
+    extern const char*   fishunique6_png;
+    const int            fishunique6_pngSize = 22397;
+
+    extern const char*   fishunique7_png;
+    const int            fishunique7_pngSize = 20364;
+
+    extern const char*   fishunique8_png;
+    const int            fishunique8_pngSize = 18308;
+
+    extern const char*   fishunique9_png;
+    const int            fishunique9_pngSize = 16351;
+
+    extern const char*   fishunique10_png;
+    const int            fishunique10_pngSize = 15563;
+
+    extern const char*   fishunique11_png;
+    const int            fishunique11_pngSize = 17076;
+
+    extern const char*   fishunique12_png;
+    const int            fishunique12_pngSize = 19194;
+
+    extern const char*   fishunique13_png;
+    const int            fishunique13_pngSize = 21920;
+
+    extern const char*   fishunique14_png;
+    const int            fishunique14_pngSize = 24285;
+
+    extern const char*   fishunique15_png;
+    const int            fishunique15_pngSize = 26017;
+
+    extern const char*   fishunique16_png;
+    const int            fishunique16_pngSize = 26460;
+
+    extern const char*   fishunique17_png;
+    const int            fishunique17_pngSize = 26887;
+
+    extern const char*   fishunique18_png;
+    const int            fishunique18_pngSize = 26985;
+
+    extern const char*   fishunique19_png;
+    const int            fishunique19_pngSize = 27068;
+
+    extern const char*   fishunique20_png;
+    const int            fishunique20_pngSize = 27675;
+
+    extern const char*   fishunique21_png;
+    const int            fishunique21_pngSize = 28454;
+
+    extern const char*   fishunique22_png;
+    const int            fishunique22_pngSize = 28636;
+
+    extern const char*   fishunique23_png;
+    const int            fishunique23_pngSize = 27591;
+
+    extern const char*   fishunique24_png;
+    const int            fishunique24_pngSize = 25430;
+
+    extern const char*   fishunique25_png;
+    const int            fishunique25_pngSize = 23076;
+
+    extern const char*   fishunique26_png;
+    const int            fishunique26_pngSize = 20825;
+
+    extern const char*   fishunique27_png;
+    const int            fishunique27_pngSize = 18780;
+
+    extern const char*   fishunique28_png;
+    const int            fishunique28_pngSize = 18240;
+
+    extern const char*   fishunique29_png;
+    const int            fishunique29_pngSize = 19361;
+
+    extern const char*   fishunique30_png;
+    const int            fishunique30_pngSize = 21683;
+
+    extern const char*   fishunique31_png;
+    const int            fishunique31_pngSize = 23699;
+
+    extern const char*   fishunique32_png;
+    const int            fishunique32_pngSize = 24777;
+
+    extern const char*   fishunique33_png;
+    const int            fishunique33_pngSize = 25945;
+
+    extern const char*   fishunique34_png;
+    const int            fishunique34_pngSize = 26784;
+
+    // Number of elements in the namedResourceList and originalFileNames arrays.
+    const int namedResourceListSize = 70;
+
+    // Points to the start of a list of resource names.
+    extern const char* namedResourceList[];
+
+    // Points to the start of a list of resource filenames.
+    extern const char* originalFilenames[];
+
+    // If you provide the name of one of the binary resource variables above, this function will
+    // return the corresponding data and its size (or a null pointer if the name isn't found).
+    const char* getNamedResource (const char* resourceNameUTF8, int& dataSizeInBytes);
+
+    // If you provide the name of one of the binary resource variables above, this function will
+    // return the corresponding original, non-mangled filename (or a null pointer if the name isn't found).
+    const char* getNamedResourceOriginalFilename (const char* resourceNameUTF8);
+}
--- Source/MP3Processor.h
+++ Source/MP3Processor.h
@@ -24,6 +24,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #include <lame.h>
 #include <stdlib.h>
 #include <memory>
+#include <limits>
 
 class MP3Processor {
 public:
