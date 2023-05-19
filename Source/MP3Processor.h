a0961f9b5bdff3e4cd7bb0b82cbc3ac39793c7cd
ArdenButterfield <76457489+ArdenButterfield@users.noreply.github.com>
2023-05-19 16:18:18 -0700

    extern c for include lame

--- Source/MP3Processor.h
+++ Source/MP3Processor.h
@@ -21,8 +21,9 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 #include <vector>
 #include <array>
+extern "C" {
 #include <lame.h>
-#include <stdlib.h>
+}
 #include <algorithm>
 #include <memory>
 #include <limits>
